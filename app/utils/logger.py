094fcc58357599554b209db7d08da6ebec9f6330
Rajanish1111 <rajanishshetty1009@gmail.com>
2025-08-13 20:59:09 +0530

    Update logger.py

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,1 +1,24 @@
+import logging
+import sys
 
+def get_logger(name: str) -> logging.Logger:
+    """
+    Initializes and returns a standardized logger.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False  # Prevent duplicate logs in Uvicorn
+
+    # If handlers are already configured, don't add them again
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            "%(asctime)s - [%(levelname)s] - %(name)s: %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
+
+# A global logger instance for easy import
+log = get_logger(__name__)
